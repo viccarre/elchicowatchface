849a2df3e345902f9f2a4f75f5061553a5d9c631
Victor Carreño <viccarre@gmail.com>
2015-09-27 16:37:37 -0400

    Battery Level added

--- src/main.c
+++ src/main.c
@@ -2,6 +2,30 @@
 
 static Window *s_main_window;
 static TextLayer *s_time_layer, *s_date_layer;
+static int s_battery_level;
+static Layer *s_battery_layer;
+
+//Battery level
+static void battery_callback(BatteryChargeState state) {
+  // Record the new battery level
+  s_battery_level = state.charge_percent;
+}
+
+static void battery_update_proc(Layer *layer, GContext *ctx) {
+  GRect bounds = layer_get_bounds(layer);
+
+  // Find the width of the bar
+  int width = (int)(float)(((float)s_battery_level / 100.0F) * 114.0F);
+
+  // Draw the background
+  graphics_context_set_fill_color(ctx, GColorBlack);
+  graphics_fill_rect(ctx, bounds, 0, GCornerNone);
+
+  // Draw the bar
+  graphics_context_set_fill_color(ctx, GColorWhite);
+  graphics_fill_rect(ctx, GRect(0, 0, width, bounds.size.h), 0, GCornerNone);
+}
+
 
 static void update_time() {
   // Get a tm structure
@@ -63,6 +87,17 @@ static void main_window_load(Window *window) {
 	
 	// Add it as a child layer to the Window's root layer
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_date_layer));
+	
+	// Create battery meter Layer
+	s_battery_layer = layer_create(GRect(14, 65, 115, 2));
+	layer_set_update_proc(s_battery_layer, battery_update_proc);
+
+	// Add to Window
+	layer_add_child(window_get_root_layer(window), s_battery_layer);
+	
+	// Update meter
+	layer_mark_dirty(s_battery_layer);
+	
 }
 
 static void main_window_unload(Window *window) {
@@ -73,6 +108,8 @@ static void main_window_unload(Window *window) {
 	// Destroy TextLayer
   text_layer_destroy(s_date_layer);
 	
+	//Destroy Battery level
+	layer_destroy(s_battery_layer);
 }
 
 static void init() {
@@ -85,12 +122,21 @@ static void init() {
     .load = main_window_load,
     .unload = main_window_unload
   });
+	
+	//Background Color of main window
+	window_set_background_color(s_main_window, GColorVividCerulean);
 
   // Show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
 	
 	// Register with TickTimerService
 	tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
+	
+	// Register for battery level updates
+	battery_state_service_subscribe(battery_callback);
+	
+	// Ensure battery level is displayed from the start
+	battery_callback(battery_state_service_peek());
 
 }
 
